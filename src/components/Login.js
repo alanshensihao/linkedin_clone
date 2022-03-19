e3651d9bc9a882cf4e2b82fbaf77282206dac3f4
alanshensihao <alanshensihao@gmail.com>
2022-03-18 20:34:29 -0400

    frontend: google sign in section added

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,12 +17,12 @@ const Login = (props) => {
           <h1>Welcome to your professional community</h1>
           <img src = "./images/login-hero.svg" alt = "" />
         </Hero>
-        <form>
+        <Form>
           <Google>
             <img src = "./images/google.svg" alt = "" />
             Sign in with Google
           </Google>
-        </form>
+        </Form>
       </Section>
 
     </Container>
@@ -122,8 +122,8 @@ const Hero = styled.div`
   }
 
   img {
-    max-width: 800px;
-    max-height: 800px;
+    max-width: 50%;
+    max-height: 100%;
     width: 700px;
     height: 670px;
     position: absolute;
@@ -140,9 +140,38 @@ const Hero = styled.div`
   }
 `;
 
-const Google = styled.button`
+const Form = styled.div`
   margin-top: 100px;
-  
+  width: 400px;
+
+  @media (max-width 768px) {
+    margin-top: 20px;
+
+  }
 `;
+
+const Google = styled.button`
+  display = flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb (0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
+  
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
+  }
+
+`;
+
 export default Login;
 
